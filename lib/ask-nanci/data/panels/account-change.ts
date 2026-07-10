1187268173cb2eecc6c57ea1f7cea905744abd10
Thuan Nguyen <nguyenducthuan133@gmail.com>
2026-07-09 19:56:53 -0500

    feat: rewrite Account Change data to match Figma reference

---
+++ lib/ask-nanci/data/panels/account-change.ts
@@ -0,0 +1,4 @@
+export const CURRENT_ACCOUNT = { last4: "4432" }
+export const NEW_ACCOUNT = { bank: "First National", last4: "7715" }
+export const CONFIRMATION_EMAIL = "...@oakst.com"
+export const CONFIRMED_AT = "3:40 PM"
